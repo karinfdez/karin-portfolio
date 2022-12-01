57783dd00b32a7f59662a9279ad17b5ef685cdbf
karinfdez <karinfdez@yahoo.com>
2022-12-01 16:14:01 -0500

    Add final details for 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,10 +8,11 @@ export default function Custom404() {
             <div className="flex flex-col h-screen justify-center py-[30px] items-center spce-y-6 xl:space-y-10 min-h-[600px] px-4">
                 <div className="flex text-white text-[8rem] xl:text-[14rem] xl:leading-[12rem] z-10">404_</div>
                 <div className="flex flex-wrap items-center justify-center space-y-6 md:space-y-0">
-                    <div className="flex text-xl text-center">
-                        <div className="flex items-center h-full w-full z-10 text-[#EC992A]">There's nothing to see here. Let's take you back</div>
+                    <div className="flex text-center">
+                        <div className="flex items-center h-full w-full z-10 text-xl 3xl:text-2xl text-[#EC992A]">There's nothing to see here. Let's take you back</div>
                     </div>
-                    <Link href="/" className="z-10 ml-2 border-2 border-[#EC992A] px-14 py-2 text-[#EC992A] text-xl hover:bg-[#EC992A] hover:text-black ease-in-out duration-500">home</Link>
+                    <Link href="/" className="flex items-center z-10 ml-2 border-2 border-[#EC992A] text-[#EC992A] text-xl 3xl:text-2xl hover:bg-[#EC992A] hover:text-black ease-in-out duration-500 w-[166px] h-[45px] px-2">
+                    <p className="w-full text-center md:text-left">home</p></Link>
                 </div>
             </div>
         </div>
