7b413874499c0b6520bdc7ed9c6360d52875af46
karinfdez <karinfdez@yahoo.com>
2022-12-01 15:40:07 -0500

    Finish 404 page styling

--- pages/404.js
+++ pages/404.js
@@ -4,17 +4,16 @@ import Header from "../components/Header"
 
 export default function Custom404() {
     return (
-        <>
-            <main className="mydiv w-full h-full xl:items-center flex flex-wrap bg-image-container">
-                <div className="flex w-full md:h-[900px] 3xl:h-[800px] justify-center items-center md:portrait:min-h-[600px]">
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-white text-[14rem]">404_</div>
-                        <div className="flex items-center">
-                            <div className="flex text-white text-xl text-[#EC992A]">You're out of your comfort zone. Let's take you back</div><button className="ml-2 border border-white px-14 py-3 text-[#EC992A] text-xl">HOME</button>
-                        </div>
+        <div className="flex w-full items-center justify-center bg-image-container">
+            <div className="flex flex-col h-screen justify-center py-[30px] items-center spce-y-6 xl:space-y-10 min-h-[600px] px-4">
+                <div className="flex text-white text-[8rem] xl:text-[14rem] xl:leading-[12rem] z-10">404_</div>
+                <div className="flex flex-wrap items-center justify-center space-y-6 md:space-y-0">
+                    <div className="flex text-xl text-center">
+                        <div className="flex items-center h-full w-full z-10 text-[#EC992A]">There's nothing to see here. Let's take you back</div>
                     </div>
+                    <Link href="/" className="z-10 ml-2 border-2 border-[#EC992A] px-14 py-2 text-[#EC992A] text-xl hover:bg-[#EC992A] hover:text-black ease-in-out duration-500">home</Link>
                 </div>
-            </main>
-        </>
+            </div>
+        </div>
     )
   }
