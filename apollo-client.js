367f69be9dda7c54ab03423475dd1d302a457e34
karinfdez <karinfdez@yahoo.com>
2023-01-22 20:39:05 -0500

    Remove contents

--- apollo-client.js
+++ apollo-client.js
@@ -1,19 +1,19 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { onError } from "@apollo/client/link/error";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-    }
-    if (networkError) {
-        console.log(`[Network error]: ${networkError}`);
-    }
-  });
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
 const httpLink = createHttpLink({
   uri: process.env.HYGRAPHCMS_URL,
 });
@@ -22,14 +22,16 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: process.env.HYGRAPH_PERMANENTAUTH_TOKEN ? `Bearer ${process.env.HYGRAPH_PERMANENTAUTH_TOKEN}` : "",
-    }
+      authorization: process.env.HYGRAPH_PERMANENTAUTH_TOKEN
+        ? `Bearer ${process.env.HYGRAPH_PERMANENTAUTH_TOKEN}`
+        : "",
+    },
   };
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export default client;
